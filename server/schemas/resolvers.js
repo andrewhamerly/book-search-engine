5a4f0257614fba54d05ae2de7bdb2e1c70153498
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-08-03 15:31:51 -0400

    Added resolvers.js for GraphQL server.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,64 @@
+const { User } = require('./models');
+const { signToken } = require('./utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return await User.find();
+    },
+    user: async (parent, { _id }) => {
+      return await User.findById(_id);
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findById(context.user._id);
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { bookInput }, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(
+          context.user._id,
+          { $addToSet: { savedBooks: bookInput } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(
+          context.user._id,
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in');
+    },
+  },
+};
+
+module.exports = resolvers;
